3eb72ed7154d2e96faf8de76e72f05fa0371e762
Eduardo Robles Elvira <edulix@gmail.com>
2021-09-13 09:52:23 +0200

    merging 4.0.0 back into master

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='agora-tools',
-    version='20.2.0',
+    version='master',
     author='Agora Voting SL',
     author_email='contact@nvotes.com',
     packages=[],
