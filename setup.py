370a02fd25f31a6f6ef8176206dd2e7aa990e4be
Eduardo Robles Elvira <edu@sequentech.io>
2022-01-05 12:06:08 +0100

    Release for version 5.0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='agora-tools',
-    version='5.0.2',
+    version='5.0.3',
     author='Agora Voting SL',
     author_email='contact@nvotes.com',
     packages=[],
