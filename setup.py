9e3e99abcdb68dc93c1f656248097c05e0ed321c
Eduardo Robles Elvira <edu@sequentech.io>
2022-04-07 12:43:40 +0200

    Release for version 6.0.0-beta.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='misc-tools',
-    version='master',
+    version='6.0.0-beta.1',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=[],
