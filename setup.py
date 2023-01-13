a4a7f9309d717ee37748cc90f7a917c5b8f2b12a
Findeton <felix@sequentech.io>
2023-01-13 12:05:14 +0100

    Release for version 7.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='misc-tools',
-    version='7.1.0',
+    version='7.1.1',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=[],
