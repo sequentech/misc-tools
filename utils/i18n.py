36726534d66aec85bb5301ea3dba2787b101b32f
Eduardo Robles <edu@sequentech.io>
2023-09-13 16:46:21 +0200

    Add italian i18n (#55)
    Parent issue: https://github.com/sequentech/meta/issues/235

---
+++ utils/i18n.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+
+def update_items(key, base, i18n, filtered):
+    typeof = type(base[key])
+    if isinstance(base[key], str):
+        filtered[key] = i18n[key]
+    else:
+        filtered[key] = dict()
+        for key2, _ in base[key].items():
+            if key2 in i18n[key]:
+                update_items(key2, base[key], i18n[key], filtered[key])
+
+def run(base_path, i18n_path, out_path):
+    base = json.loads(open(base_path).read())
+    i18n = json.loads(open(i18n_path).read())
+    filtered = dict()
+    for key, _ in base.items():
+        if key in i18n:
+            update_items(key, base, i18n, filtered)
+    open(out_path,mode="w").write(json.dumps(filtered, indent=2,sort_keys=True))
+
+def main():
+    '''
+    Main function
+    '''
+    parser = argparse.ArgumentParser(prog='i18n.py')
+    parser.add_argument(
+        "--base",
+        help="Base translation file path"
+    )
+    parser.add_argument(
+        "--i18n",
+        help="i18n translation file path"
+    )
+    parser.add_argument(
+        "--out",
+        help="Output translation file path"
+    )
+    pargs = parser.parse_args()
+
+    run(pargs.base, pargs.i18n, pargs.out)
+
+main()
--- utils/remove_backups.py
+++ utils/remove_backups.py
@@ -3,54 +3,21 @@
 import json
 import argparse
 import subprocess
+from datetime import datetime, timedelta, timezone
 
-def main():
-    '''
-    Main function
-    '''
-    parser = argparse.ArgumentParser(prog='remove_backups.py')
-    parser.add_argument(
-        "--vol-id",
-        help="Volume Id, example: vol-194f68ea"
-    )
-    parser.add_argument(
-        "--aws-bin-path",
-        help="Path to aws binary",
-        default="/usr/bin/aws"
-    )
-    parser.add_argument(
-        "--base-arn",
-        help="Base ARN",
-        default="arn:aws:ec2:eu-west-1:498439606504:volume/"
-    )
-    parser.add_argument(
-        "--vol-list-file",
-        help="File with a list of volumes, one per line, example: file.csv"
-    )
-    parser.add_argument(
-        "--exclude-snap-ids",
-        nargs='*',
-        help="Ids from snapshot that will not be removed",
-        default=[]
-    )
-    parser.add_argument(
-        "--remove-all",
-        help="Force to remove all",
-        action="store_true"
-    )
-    parser.add_argument(
-        "--print-only",
-        help="Do not execute any action, just print what would be done",
-        action="store_true"
-    )
-    pargs = parser.parse_args()
+def get_days_old(date, now):
+    return (now - datetime.fromisoformat(date)).days
+
+def use_backups(pargs, parser):
     vol_id = pargs.vol_id
     vol_list_file = pargs.vol_list_file
-    exclude_snap_ids = pargs.exclude_snap_ids
     print_only = pargs.print_only
-    remove_all = pargs.remove_all
     base_arn = pargs.base_arn
     aws_bin_path = pargs.aws_bin_path
+    maintain_days = pargs.maintain_days
+
+    now = datetime.now(timezone.utc)
+    oldest_allowed_date = now - timedelta(days=maintain_days)
 
     if not vol_id and not vol_list_file:
         parser.print_help()
@@ -82,45 +49,35 @@ def main():
         )
         recovery_points_list = json.loads(recovery_points_list_json)['RecoveryPoints']
 
-        # filter those with less than a year of expiration
-        if not remove_all:
-            limit = 365
-        else:
-            limit = 10000
-
-        short_recovery_points_list = [
-            point['RecoveryPointArn']
+        to_remove_recovery_points_list = [
+            point
             for point in recovery_points_list
-            if (
-                isinstance(point["Lifecycle"], dict) and
-                point["Lifecycle"]["DeleteAfterDays"] < 365 and
-                point['RecoveryPointArn'] not in exclude_snap_ids
-            )
+            if datetime.fromisoformat(point['CreationDate']) <= oldest_allowed_date
         ]
-        long_recovery_points_list = [
-            point['RecoveryPointArn']
+        to_maintain_recovery_points_list = [
+            point
             for point in recovery_points_list
-            if (
-                isinstance(point["Lifecycle"], dict) and
-                point["Lifecycle"]["DeleteAfterDays"] >= 365
-            )
+            if datetime.fromisoformat(point['CreationDate']) > oldest_allowed_date
         ]
         print("To remove:")
-        print("\n".join(short_recovery_points_list))
+        for point in to_remove_recovery_points_list:
+            days_old = get_days_old(point['CreationDate'], now)
+            arn = point['RecoveryPointArn']
+            print(f"- {arn} ({days_old} days old)")
+
         print("\nTo maintain:")
-        print("\n".join(long_recovery_points_list))
+        for point in to_maintain_recovery_points_list:
+            days_old = get_days_old(point['CreationDate'], now)
+            arn = point['RecoveryPointArn']
+            print(f"- {arn} ({days_old} days old)")
 
-        if len(long_recovery_points_list) == 0 and not remove_all:
-            print("Failing, no recovery point would be maintained")
-            exit(1)
-
-        print("\nStarting to remove:")
+        print("\nStarting to remove..")
 
         # remove short recovery points
         if print_only:
-            print("not removing anything as we are in print only mode")
+            print("..not removing anything as we are in print only mode. Finished.")
             exit(0)
-        for point in short_recovery_points_list:
+        for point in to_remove_recovery_points_list:
             cmd = [
                 aws_bin_path,
                 "backup",
@@ -128,14 +85,132 @@ def main():
                 "--backup-vault-name",
                 "Default",
                 "--recovery-point-arn",
-                point
+                point['RecoveryPointArn']
             ]
             ret = subprocess.check_call(cmd)
             if ret == 0:
                 print("removed point %s" % point)
             else:
                 print("error removing point %s, command = cmd" % (
-                    (point, " ".join(cmd))
+                    (point['RecoveryPointArn'], " ".join(cmd))
                 ))
+
+def use_snapshots(pargs, _parser):
+    vol_id = pargs.vol_id
+    vol_list_file = pargs.vol_list_file
+    print_only = pargs.print_only
+    base_arn = pargs.base_arn
+    aws_bin_path = pargs.aws_bin_path
+    maintain_days = pargs.maintain_days
+
+    now = datetime.now(timezone.utc)
+    oldest_allowed_date = now - timedelta(days=maintain_days)
+
+    if vol_id:
+        vol_list = [vol_id]
+    else:
+        with open(vol_list_file, 'r') as f:
+            vol_list = [
+                line.replace("\n", "")
+                for line in open("vol-ids.txt", "r").readlines()
+                if len(line) > 0
+            ]
+    
+    if len(vol_list) > 0:
+        vol_list_str = "[?contains(" + json.dumps(vol_list) + ", VolumeId)]&&"
+    else:
+        vol_list_str = ""
+
+    oldest_date_str = oldest_allowed_date.isoformat()
+
+    command = [
+        aws_bin_path,
+        "ec2",
+        "describe-snapshots",
+        "--output",
+        "json",
+        "--query",
+        f"Snapshots{vol_list_str}[?StartTime<\"{oldest_date_str}\"].[SnapshotId,StartTime]"
+    ]
+    print(f"executing: {command}..")
+    snapshots_ids_json = subprocess.check_output(command)
+    print("command output: {snapshots_ids_json}")
+
+    snapshot_ids = json.loads(snapshots_ids_json)
+    print("To remove:")
+    for snapshot in snapshot_ids:
+            days_old = get_days_old(snapshot['CreationDate'], now)
+            snapshot_id = snapshot['SnapshotId']
+            print(f"- {snapshot_id} ({days_old} days old)")
+
+    print("\nStarting to remove..")
+
+    # remove short recovery points
+    if print_only:
+        print("..not removing anything as we are in print only mode. Finished.")
+    exit(0)
+    for snapshot in snapshot_ids:
+        '''cmd = [
+            aws_bin_path,
+            "backup",
+            "delete-recovery-point",
+            "--backup-vault-name",
+            "Default",
+            "--recovery-point-arn",
+            point['RecoveryPointArn']
+        ]
+        ret = subprocess.check_call(cmd)
+        if ret == 0:
+            print("removed point %s" % point)
+        else:
+            print("error removing point %s, command = cmd" % (
+                (point['RecoveryPointArn'], " ".join(cmd))
+            ))'''
+
+def main():
+    '''
+    Main function
+    '''
+    parser = argparse.ArgumentParser(prog='remove_backups.py')
+    parser.add_argument(
+        "--vol-id",
+        help="Volume Id, example: vol-194f68ea"
+    )
+    parser.add_argument(
+        "--aws-bin-path",
+        help="Path to aws binary",
+        default="/usr/bin/aws"
+    )
+    parser.add_argument(
+        "--base-arn",
+        help="Base ARN",
+        default="arn:aws:ec2:eu-west-1:498439606504:volume/"
+    )
+    parser.add_argument(
+        "--vol-list-file",
+        help="File with a list of volumes, one per line, example: file.csv"
+    )
+    parser.add_argument(
+        "--print-only",
+        help="Do not execute any action, just print what would be done",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--use-snapshots",
+        help="Instead of using backups, use snapshots",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--maintain-days",
+        help="number of days to maintain",
+        type=int,
+        default=365
+    )
+    pargs = parser.parse_args()
+    
+    if pargs.use_snapshots:
+        use_snapshots(pargs, parser)
+    else:
+        use_backups(pargs, parser)
 
 main()
