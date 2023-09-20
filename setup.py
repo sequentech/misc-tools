840b4e36d11c02818974c235f94c6e5ea84c5f5d
Eduardo Robles Elvira <edu@sequentech.io>
2023-09-20 17:33:28 +0200

    Release for version 10.0.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='misc-tools',
-    version='master',
+    version='10.0.0',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=[],
